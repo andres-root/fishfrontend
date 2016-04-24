f4dcd3305fe29faa842076eddc0938e9f1ca0a89
andres-root <andres.root.coder@gmail.com>
2016-04-24 00:56:12 -0500

    working in some wizardy shit

--- www/classifier.py
+++ www/classifier.py
@@ -6,12 +6,14 @@ from .models import Fish
 class Classifier(object):
 
     def __init__(self):
-        data = Fish.objects.values('size', 'color', 'name')
-        self.X = []
-        self.y = []
-        for e in data:
-            self.X.append([e['size'], e['color']])
-            self.y.append(e['name'])
+        # data = Fish.objects.values('size', 'color', 'name')
+        # self.X = []
+        # self.y = []
+        # for e in data:
+        #     self.X.append([e['size'], e['color']])
+        #     self.y.append(e['name'])
+        self.X = [[10, 1], [11, 1], [2, 0], [9, 1], [6, 0], [9, 1], [6, 0], [5, 0], [2, 0], [12, 0], [10, 1], [14, 1]]
+        self.y = ['mojarra', 'mojarra', 'bagre', 'mojarra', 'bagre', 'mojarra', 'bagre', 'bagre', 'bagre', 'bagre', 'mojarra', 'mojarra']
         self.clf = svm.SVC()
 
     def train(self):
